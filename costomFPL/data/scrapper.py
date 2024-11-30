934a43712d4a0a53295e5d7b285f40467b578594
6076paras <47757847+6076paras@users.noreply.github.com>
2024-11-30 09:02:56 -0800

    all links

--- costomFPL/data/scrapper.py
+++ costomFPL/data/scrapper.py
@@ -9,6 +9,7 @@ def make_URL(config):
     Generate URL to scrape from:
     """
     print("Select data...")
+    # TODO: add more catagories, promt error for invalid input
     season = input("Which season? [2024-2025, 2023-2022]: ")
     league = input("League? [Premier-League, Bundesliga]: ")
 
@@ -23,6 +24,18 @@ def make_URL(config):
     return url, season, league
 
 
+def soup_URL(url, season, league):
+    """
+    Collect links for every match in a season
+    """
+    # all links
+    req_obj = requests.get(url)
+    parse_html = BeautifulSoup(req_obj.content, "html.parser")
+    all_links = parse_html.find_all("a")
+
+    return
+
+
 def test_pandas(config):
     for url in config["url"]:
         tables = pd.read_html(url)
@@ -35,4 +48,5 @@ if __name__ == "__main__":
     path = "/Users/paraspokharel/Programming/costomFPL/costomFPL/config/dataVars.yaml"
     with open(path, "r") as file:
         config = yaml.safe_load(file)
-    make_URL(config)
+    url, season, league = make_URL(config)
+    soup_URL(url, season, league)
