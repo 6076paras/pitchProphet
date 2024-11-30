95698dff9f20ff8c525f31c284bcba4cfef89968
6076paras <47757847+6076paras@users.noreply.github.com>
2024-11-30 10:46:17 -0800

    filter match links

--- costomFPL/data/scrapper.py
+++ costomFPL/data/scrapper.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 import requests
 import yaml
@@ -20,7 +22,6 @@ def make_URL(config):
         league_id = "20"
 
     url = f"https://fbref.com/en/comps/{league_id}/{season}/schedule/{season}-{league}-Scores-and-Fixtures"
-
     return url, season, league
 
 
@@ -33,7 +34,15 @@ def soup_URL(url, season, league):
     parse_html = BeautifulSoup(req_obj.content, "html.parser")
     all_links = parse_html.find_all("a")
 
-    return
+    # links form score
+    # TODO: check repetation
+    relv_keys = [f"{league}", "/en/matches/"]
+    match_links = []
+    for link in all_links:
+        href = link.get("href", "")
+        if all(relv_key in href for relv_key in relv_keys):
+            match_links.append("https://fbref.com" + href)
+    return match_links
 
 
 def test_pandas(config):
@@ -48,5 +57,9 @@ if __name__ == "__main__":
     path = "/Users/paraspokharel/Programming/costomFPL/costomFPL/config/dataVars.yaml"
     with open(path, "r") as file:
         config = yaml.safe_load(file)
-    url, season, league = make_URL(config)
-    soup_URL(url, season, league)
+    url, league, season = (
+        "https://fbref.com/en/comps/9/Premier-League/schedule/2023-2024-Premier_League-Scores-and-Fixtures",
+        "Premier-League",
+        "2024-2025",
+    )
+    print(soup_URL(url, season, league)[0:5])
