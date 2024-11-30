f522955e6f744b894deefd429e64de64ad626ed5
6076paras <47757847+6076paras@users.noreply.github.com>
2024-11-29 22:58:13 -0800

    precommit mypy types remove

--- costomFPL/data/scrapper.py
+++ costomFPL/data/scrapper.py
@@ -1,28 +1,29 @@
 import pandas as pd
-
-# import requests
+import requests
 import yaml
+from bs4 import BeautifulSoup
 
-# from bs4 import BeautifulSoup
 
-
-def returnURL(config):
-    return NotImplemented
+def make_URL(config):
+    """
+    Generate URL to scrape from:
+    """
+    print("Select data...")
+    season = input("Which season? [2024-2025, 2023-2022]: ")
+    league = input("League? [Premier League, Bundesliga]: ")
+    return season, league
 
 
 def test_pandas(config):
     for url in config["url"]:
-        data = pd.read_html(url)
-        print(data[0])
+        tables = pd.read_html(url)
+        for table in tables:
+            print(table.dtypes)
     return
 
 
 if __name__ == "__main__":
-
-    path = (
-        "/Users/paraspokharel/Programming/costomFPL/" "costomFPL/config/dataVars.yaml"
-    )
+    path = "/Users/paraspokharel/Programming/costomFPL/costomFPL/config/dataVars.yaml"
     with open(path, "r") as file:
         config = yaml.safe_load(file)
-
-    a = test_pandas(config)
+    make_URL(config)
