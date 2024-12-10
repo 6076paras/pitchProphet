54f2be1dd202f6a10793b78a0b3d6a63a3062b21
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-09 20:18:52 -0800

    css styling

--- web/app.py
+++ web/app.py
@@ -14,8 +14,7 @@ def fetch_fixture():
 def mk_table():
     data = test_prediction.generate_ml_out()
     df = pd.DataFrame(data)
-    html_table = df.to_html(classes="table table-striped")
-
+    html_table = df.to_html(classes="table table-striped shadow table-hover")
     return render_template("index.html", table=html_table)
 
 
