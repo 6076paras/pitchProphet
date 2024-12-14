df987bc47ce0b731995b0d33a19a0d9d60276899
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-14 15:26:08 -0800

    cleanup

--- costomFPL/data/pre_processing/pre_process.py
+++ costomFPL/data/pre_processing/pre_process.py
@@ -4,15 +4,6 @@ import pandas as pd
 import yaml
 
 
-def flattan_json(path: str) -> pd.DataFrame:
-    """
-    Loads json file into pd.Dataframe
-    return pd.read_csv(path)
-    by flattening.
-    """
-    pass
-
-
 def calc_stats(n, data):
     """
     Dataframe map that generates statistics, f(data_i) using last its last n
@@ -27,34 +18,37 @@ def calc_stats(n, data):
     pass
 
 
-def extract_relv(data: pd.DataFrame) -> pd.DataFrame:
-    """
-    Extract relevent data
-    """
-    pass
+def game_data_process(data: dict) -> pd.DataFrame:
 
-
-def main():
-    json_path = "/Users/paraspokharel/Programming/costomFPL/costomFPL/data/fbref/2023-2024-Premier-League-3-matches.json"
-    with open(json_path, "r") as file:
-        data = json.load(file)
-    normalized = pd.json_normalize(data)
-    print(normalized.columns)
-    # Normalize the data
-
-    # Flatten HomeStat
+    # flatten dataset
     home_stat_df = pd.json_normalize(data, record_path="HomeStat")
     away_stat_df = pd.json_normalize(data, record_path="AwayStat")
     game_data_df = pd.json_normalize(data, record_path="MatchInfo")
 
-    # Add the level for first index based on the section name
+    # add first level index to make it multi index obj
     home_stat_df.index = pd.MultiIndex.from_product([["HomeStat"], home_stat_df.index])
     away_stat_df.index = pd.MultiIndex.from_product([["AwayStat"], away_stat_df.index])
     game_data_df.index = pd.MultiIndex.from_product([["MatchInfo"], game_data_df.index])
 
-    # Now, concatenate all DataFrames into one with MultiIndex
-    final_df = pd.concat([game_data_df, home_stat_df, away_stat_df])
-    print(final_df)
+    return pd.concat([game_data_df, home_stat_df, away_stat_df])
+
+
+def open_json(file_path: str) -> dict:
+    with open(json_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def main():
+    json_path = "/Users/paraspokharel/Programming/costomFPL/costomFPL/data/fbref/2023-2024-Premier-League-3-matches.json"
+
+    # convert json to python dict
+    data = open_json(json_path)
+
+    # convert json game data into multi-index dataframe
+    data = game_data_process(data)
+
+    #
 
 
 if __name__ == "__main__":
