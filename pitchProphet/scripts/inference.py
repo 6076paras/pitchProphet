fd88ad4a12aa4eae93a224fb6f51ed7172f3945b
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-25 21:33:03 -0800

    refactor

--- pitchProphet/scripts/inference.py
+++ pitchProphet/scripts/inference.py
@@ -1,32 +1,55 @@
+from typing import List
+
 import pandas as pd
 import yaml
 
 from pitchProphet.data.fbref.fbref_scrapper import FBRefScraper
 
 
-def get_fixtures(match_week, url):
-    """
-    Returns a fixture list from FBRef
-    """
+def get_fixtures(match_week: int, url: str) -> pd.DataFrame:
+    """returns a fixture list from FBRef for a given match week."""
+    try:
+        all_fixtures = pd.read_html(url)
+        week_fixtures = all_fixtures[0][all_fixtures[0]["Wk"] == match_week]
+        team_list = week_fixtures[["Home", "Away"]].reset_index(drop=True)
+        team_list.name = f"Matchweek {match_week}"
+        return team_list
+    except Exception as e:
+        print(f"Error fetching fixtures: {e}")
+        return pd.DataFrame()
 
-    all_fixtures = pd.read_html(url)
-    week_fixtures = all_fixtures[0][all_fixtures[0]["Wk"] == match_week]
-    team_list = week_fixtures[["Home", "Away"]].reset_index()
-    team_list.name = f"Matchweek {match_week}"
-    return team_list
+
+def load_config(path: str) -> dict:
+    """loads the configuration from a YAML file."""
+    try:
+        with open(path, "r") as file:
+            config = yaml.safe_load(file)
+        return config
+    except Exception as e:
+        print(f"Error loading config: {e}")
+        return {}
 
 
 def main():
     config_path = (
         "/Users/paraspokharel/Programming/pitchProphet/pitchProphet/config/config.yaml"
     )
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-    config = config["scraper"]
-    league = "Premier-League"
-    league_id = config["league_ids"][league]
-    url = f"{config["base_url"]}/{league_id}/2024-2025/schedule/2024-2025-{league}-Scores-and-Fixtures"
-    print(get_fixtures(1, url))
+    config = load_config(config_path)
+    if not config:
+        print("Failed to load configuration.")
+        return
+
+    try:
+        league = "Premier-League"
+        league_id = config["scraper"]["league_ids"][league]
+        url = f"{config['scraper']['base_url']}/{league_id}/2024-2025/schedule/2024-2025-{league}-Scores-and-Fixtures"
+        fixtures = get_fixtures(1, url)
+        if not fixtures.empty:
+            print(fixtures)
+        else:
+            print("No fixtures found.")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
 
 
 if __name__ == "__main__":
