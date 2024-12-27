db94db114fd39f3787b8fdab9b37d792c9ca260d
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-26 16:57:55 -0800

    import table from assets

--- web/app.py
+++ web/app.py
@@ -1,27 +1,34 @@
-# import test_prediction
 import sys
+from pathlib import Path
 
 import pandas as pd
-
-sys.path.append("../tests")
-import test_prediction
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-# fetch fixture
-def fetch_fixture():
-    pass
+def get_predictions() -> pd.DataFrame:
+    """load match predictions from CSV file."""
+    file_path = Path(
+        "/Users/paraspokharel/Programming/pitchProphet/web/static/assets/tables/Premier-League_week_10_predictions.csv"
+    )
+
+    df = pd.read_csv(file_path)
+    # Round probabilities to 3 decimal places
+    prob_columns = [col for col in df.columns if col.startswith("p(")]
+    df[prob_columns] = df[prob_columns].round(3)
+
+    return df
 
 
 @app.route("/")
 def mk_table():
-    data = test_prediction.generate_ml_out()
-    df = pd.DataFrame(data)
-    html_table = df.to_html(classes="table table-striped shadow table-hover")
+    df = get_predictions()
+    html_table = df.to_html(
+        classes="table table-striped shadow table-hover", index=False
+    )
     return render_template("index.html", table=html_table)
 
 
 if __name__ == "__main__":
-    app.run(debug="True", port=5001)
+    app.run(debug=True, port=5001)
