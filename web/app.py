b66fbe46855cd9967483e593a0c6a193a01c19ab
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-28 00:18:14 -0800

    add utility to handle all predictions

--- web/app.py
+++ web/app.py
@@ -1,33 +1,15 @@
-import sys
-from pathlib import Path
-
-import pandas as pd
 from flask import Flask, render_template
+
+from web.utils.utils import get_predictions
 
 app = Flask(__name__)
 
 
-def get_predictions() -> pd.DataFrame:
-    """load match predictions from CSV file."""
-    file_path = Path(
-        "/Users/paraspokharel/Programming/pitchProphet/web/static/assets/tables/Premier-League_week_10_predictions.csv"
-    )
-
-    df = pd.read_csv(file_path)
-    # Round probabilities to 3 decimal places
-    prob_columns = [col for col in df.columns if col.startswith("p(")]
-    df[prob_columns] = df[prob_columns].round(3)
-
-    return df
-
-
 @app.route("/")
 def mk_table():
-    df = get_predictions()
-    html_table = df.to_html(
-        classes="table table-striped shadow table-hover", index=False
-    )
-    return render_template("index.html", table=html_table)
+    """render the main page with prediction tables for all leagues."""
+    league_data = get_predictions()
+    return render_template("index.html", league_data=league_data)
 
 
 if __name__ == "__main__":
