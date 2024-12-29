0e26f9c80e9999c55b4bf1e64ce93946cec5b960
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-28 16:44:50 -0800

    import date and matchweek data

--- pitchProphet/utils/matchweek_date.py
+++ pitchProphet/utils/matchweek_date.py
@@ -4,3 +4,27 @@ Utility function that does the following based on data/match_dates/*.csv
     2. Find if there are active matches
     3. Retrun a list of leagues with no active matches
 """
+
+from pathlib import Path
+
+import pandas as pd
+
+
+def get_current_matchweek():
+    f_path = Path(
+        "/Users/paraspokharel/Programming/pitchProphet/pitchProphet/data/match_dates/matchweek_dates_2024_2025.csv"
+    )
+    df = pd.read_csv(f_path)
+    df["start_date"] = pd.to_datetime(df["start_date"])
+    df["end_date"] = pd.to_datetime(df["end_date"])
+
+    return
+
+
+def main():
+    get_current_matchweek()
+    return
+
+
+if __name__ == "__main__":
+    main()
