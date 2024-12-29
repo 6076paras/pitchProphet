7f3e8c83074983de4dd9469662987c346ab9cd70
6076paras <47757847+6076paras@users.noreply.github.com>
2024-12-28 16:48:41 -0800

    iterate and check if match week contains the date

--- pitchProphet/utils/matchweek_date.py
+++ pitchProphet/utils/matchweek_date.py
@@ -11,12 +11,29 @@ import pandas as pd
 
 
 def get_current_matchweek():
+    """get current matchweek for all leageus from date"""
     f_path = Path(
         "/Users/paraspokharel/Programming/pitchProphet/pitchProphet/data/match_dates/matchweek_dates_2024_2025.csv"
     )
+    # load data
     df = pd.read_csv(f_path)
     df["start_date"] = pd.to_datetime(df["start_date"])
     df["end_date"] = pd.to_datetime(df["end_date"])
+
+    # find current date
+    today = pd.Timestamp.now()
+    print(today)
+
+    current_weeks = {}
+    for league in df["league"].unique():
+        league_df = df[df["league"] == league]
+
+        # find the match week that contains today's date
+        current_week = None
+        for _, row in league_df.iterrows():
+            if row["start_date"] <= today <= row["end_date"]:
+                current_week = row["match_week"]
+                break
 
     return
 
